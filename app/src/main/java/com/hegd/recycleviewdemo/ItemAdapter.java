0b08a693efd2ff0e5abe2d1bd839dfef9e31106b
1136066873 <1136066873@qq.com>
2019-04-19 01:04:37 +0800

    2019_0419_0104_make_recyleview_item_can_be_clicked

--- app/src/main/java/com/hegd/recycleviewdemo/ItemAdapter.java
+++ app/src/main/java/com/hegd/recycleviewdemo/ItemAdapter.java
@@ -14,14 +14,24 @@ import java.util.List;
  * Created by 何国栋 on 2019/4/18.
  */
 
-public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder>
+        implements View.OnClickListener{
 
     public Context mContext;
     public List<String> mDatas;
+    public onRecyclerViewItemClickListener mItemClickListener = null;
 
     public ItemAdapter(Context context,List<String> datas) {
         this.mContext = context;
         this.mDatas = datas;
+    }
+
+    /**
+     * 暴露给别人可以设置监听
+     * @param listener
+     */
+    public void setOnItemClickListener(onRecyclerViewItemClickListener listener) {
+        this.mItemClickListener = listener;
     }
 
     @NonNull
@@ -29,6 +39,8 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
     public ItemAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         //加载布局并转化为 View
         View itemView = LayoutInflater.from(mContext).inflate(R.layout.layout_item, parent, false);
+        //设置监听
+        itemView.setOnClickListener(this);
         //将 View 传递给 RecycleView 封装好的 ViewHolder
         ViewHolder holder = new ViewHolder(itemView);
         return holder;
@@ -38,11 +50,21 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
     public void onBindViewHolder(@NonNull ItemAdapter.ViewHolder holder, int position) {
         //用数据源的元素代替此 position 上的内容
         holder.tv.setText(mDatas.get(position));
+
+        //把当前的位置以 TAG 的形式发送出去，方便在点击事件的时候使用这个位置
+        holder.itemView.setTag(mDatas.get(position));//将数据保存在itemView的Tag中，以便点击时进行获取
     }
 
     @Override
     public int getItemCount() {
         return mDatas.size();
+    }
+
+    @Override
+    public void onClick(View itemView) {
+        if (mItemClickListener != null){
+            mItemClickListener.onItemClick(itemView,(String)itemView.getTag());
+        }
     }
 
 
@@ -52,5 +74,10 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
             super(itemView);
             tv = itemView.findViewById(R.id.id_num);
         }
+    }
+
+    //添加 item 的点击事件回调
+    public static interface onRecyclerViewItemClickListener {
+        void onItemClick(View view,String data);
     }
 }
--- app/src/main/java/com/hegd/recycleviewdemo/MainActivity.java
+++ app/src/main/java/com/hegd/recycleviewdemo/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 //https://juejin.im/post/5a2907136fb9a0450e7603b2
 //https://www.jianshu.com/p/c3c6c81adfbc
@@ -25,7 +28,14 @@ public class MainActivity extends AppCompatActivity {
         initData();
         mAdapter = new ItemAdapter(MainActivity.this,mDatas);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mAdapter.setOnItemClickListener(new ItemAdapter.onRecyclerViewItemClickListener() {
+            @Override
+            public void onItemClick(View view, String data) {
+                Toast.makeText(MainActivity.this, "which item is clicked  -> " + data, Toast.LENGTH_SHORT).show();
+            }
+        });
         mRecyclerView.setAdapter(mAdapter);
+
     }
 
     private void initData() {
